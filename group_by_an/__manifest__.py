5d89dd3ed2573601661b67198a1f025125d04400
NVBetagroup <support@betagroupnv.com>
2024-10-22 11:10:27 -0300

    adding app

---
+++ group_by_an/__manifest__.py
@@ -0,0 +1,23 @@
+
+
+{
+    'name': 'Odoo 17 Group By Analytic Accounts',
+    'version': '2.0.2',
+    'category': 'Accounting',
+    'summary': '',
+    'description': '',
+    'live_test_url': '',
+    'sequence': '1',
+    'website': '',
+    'author': 'Tanisha',
+    'maintainer': 'RPBG',
+    'license': 'LGPL-3',
+    'support': '',
+    'depends': ['account'],
+    #'data': [
+    #
+   # ],
+    'application': True,
+
+}
+
